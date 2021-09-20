99ea81499112849d014d7478c8b8922862552456
nkbrown503 <62954304+nkbrown503@users.noreply.github.com>
2021-09-20 16:17:39 -0400

    Update FEA_SOLVER_GENERAL.py

--- Top_Opt_RL/DQN/FEA_SOLVER_GENERAL.py
+++ Top_Opt_RL/DQN/FEA_SOLVER_GENERAL.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  3 15:23:01 2021
-
-@author: nbrow
-"""
 
 import numpy as np
-import skimage.measure as measure
 
 'Define the Function that creates the rectangular mesh'
-def label( x): return measure.label(x, connectivity=1)
 
 def get_zero_label(d, labels):
     for i in range(d.shape[0]):
@@ -21,7 +13,7 @@ def isolate_largest_group_original(x):
     """Isolates the largest group. 
     this original version has problems with periodicity. The v2 version fixes this. """
     x_original = np.copy(x)    
-    labels= label(x)
+    labels= x
     zerolabel=get_zero_label(x,labels)
     group_names = np.unique(labels)      
     group_names = [g for g in group_names if g!=zerolabel]   
